a4c0a48e170fa53ddb31d1e2b0dd0f605d516947
wisdomedet06-ux <wisdomedet06@gmail.com>
2026-09-25 22:32:58 +0100

    Refresh PWA cache for latest app navigation

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE="moneymap-shell-v1";
+const CACHE="moneymap-shell-v3";
 const SHELL=["/","/index.html","/manifest.webmanifest","/icon-192.svg","/icon-512.svg"];
 self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()))});
 self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
