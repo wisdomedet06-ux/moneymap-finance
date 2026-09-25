e141d54edf1aa51998f46278fe9c06867354ab0a
wisdomedet06-ux <wisdomedet06@gmail.com>
2026-09-25 20:39:19 +0100

    Add MoneyMap offline app shell

---
+++ sw.js
@@ -0,0 +1,14 @@
+const CACHE="moneymap-shell-v1";
+const SHELL=["/","/index.html","/manifest.webmanifest","/icon-192.svg","/icon-512.svg"];
+self.addEventListener("install",event=>{event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()))});
+self.addEventListener("activate",event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()))});
+self.addEventListener("fetch",event=>{
+  const url=new URL(event.request.url);
+  if(url.origin!==self.location.origin)return;
+  if(event.request.method!=="GET")return;
+  event.respondWith(fetch(event.request).then(response=>{
+    const copy=response.clone();
+    caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+    return response;
+  }).catch(()=>caches.match(event.request).then(cached=>cached||caches.match("/index.html"))));
+});
